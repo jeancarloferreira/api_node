a7aaaf59a90598c6e19f4226d0a4cf767052ed7b
jeancarloferreira <jean.carlo.ferreira@outlook.com>
2019-11-27 21:35:07 -0300

    criando estrutura da api com sequelize

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  plugins: ['prettier'],
+
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    //retornar erro para nós
+    "prettier/prettier": "error",
+    //não usar o this. na classe
+    "class-methods-use-this": "off",
+    //quando receber um valor de parametro poder modificar ela
+    "no-param-reassingn": "off",
+    //desabilitar o padrão da declaração da variável exemplo deVar
+    "camelcase": "off",
+    //abrir exeção para declarar a variável NEXT sem utilizar
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next" }],
+  },
+};
